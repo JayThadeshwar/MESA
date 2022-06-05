e41be3289cdab95b5fdd95ca8e244c9226e37e08
JayThadeshwar <jaythadeshwar007@gmail.com>
2022-06-05 23:08:32 +0530

    User, Chapter and Keyword API done.

---
+++ Mesa/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import url
+from Mesa import views
+from django.urls import include, path
+from rest_framework import routers
+
+router = routers.DefaultRouter(trailing_slash=False)
+router.register(r'chapters', views.ChapterViewSet)
+
+urlpatterns=[
+     path('', include(router.urls)),
+     url(r'^user$',views.userApi),
+     url(r'^user/validate$',views.validateUserApi),
+     url(r'^keyword/(?P<chapter_id>\d+)/$', views.keywordApi, name='keyword')
+]
---
+++ Mesa/views.py
@@ -0,0 +1,67 @@
+from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from sqlalchemy import null
+from rest_framework.parsers import JSONParser
+from django.http.response import JsonResponse
+
+from Mesa.models import User
+from Mesa.extract_keywords import extractKeywordsFromContent
+from Mesa.models import Chapter, User
+from Mesa.serializers import UserSerializer, ChapterSerializer
+from rest_framework import viewsets
+
+# Create your views here.
+
+class ChapterViewSet(viewsets.ModelViewSet):
+    queryset = Chapter.objects.all()
+    serializer_class = ChapterSerializer
+
+@csrf_exempt
+def userApi(request, id=0):
+    if request.method == 'GET':
+        users = User.objects.all()
+        users_serializer = UserSerializer(users, many = True)
+        return JsonResponse(users_serializer.data, safe=False)
+    elif request.method == 'POST':
+        user_info = JSONParser().parse(request)
+        users_serializer = UserSerializer(data = user_info)
+        if users_serializer.is_valid():
+            users_serializer.save()
+            return JsonResponse("User information added successfully.", safe=False)
+        return JsonResponse("Failed to add information.", safe=False)
+    elif request.method == 'PUT':
+        user_info = JSONParser().parse(request)
+        user = User.objects.get(UserId = user_info['userId'])
+        users_serializer = UserSerializer(user, data = user_info)
+        if users_serializer.is_valid():
+            users_serializer.save()
+            return JsonResponse("Update successfully.", safe=False)
+        return JsonResponse("Failed to update.", safe=False)
+
+@csrf_exempt
+def validateUserApi(request, id=0):
+    if request.method == 'POST':
+        user_info = JSONParser().parse(request)
+        
+        isValid = True
+        msg = "Login successfully"
+        info = None
+
+        try:
+            user = User.objects.get(emailId = user_info['emailId'], password = user_info['password'])
+            info = UserSerializer(user, many = False).data
+        except:
+            isValid = False
+            msg = "Either username or password is invalid"            
+        
+        resp = {"msg": msg, "isValid": isValid, "info": info}                    
+        return JsonResponse(resp, safe=False)        
+
+@csrf_exempt
+def keywordApi(request, chapter_id):
+    if request.method=='GET':
+        chapter=Chapter.objects.get(id = chapter_id)                    
+        result = extractKeywordsFromContent(chapter.content)   
+        finalRes = {"keywords": result}     
+        return JsonResponse(finalRes, safe=False)
+
